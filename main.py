3ef5b6a16a2643d4c257b24708982b0c07ca57c4
MarcIMP08 <115101741+MarcIMP08@users.noreply.github.com>
2022-10-05 18:36:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import time
 print("Hello World")
 time.sleep(100)
 
-print("Das ist ein Test! Ich habe das in VSCode bearbeitet!")
+print("Das ist ein Test! Ich habe das in VSCode bearbeitet! Jetzt in GitHub")
