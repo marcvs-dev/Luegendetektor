33bb1ecfb84dc492aee5ed58a3193b4779779bf3
Marc <schule.marc08@gmail.com>
2022-10-07 15:24:44 +0200

    Revert "Updatge"
    This reverts commit e6f71dd9e3176f24c0e774af2a179a871e02db2f.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
- #--------------------------------------------------------------------------------------------------------- 
- #imports
 from csv import excel
 import time
 import pandas as pd
@@ -8,7 +6,6 @@ import os
 
  #--------------------------------------------------------------------------------------------------------- 
 
-#Excel
 
 excel_datei = pd.read_excel("dateien\Mappe1.xlsx") #Fragen und Wichtigkeit
 
@@ -17,7 +14,6 @@ print(excel_datei.iloc[1, 2]) #Zeile 1 Spalte 2 -- Es fängt bei 0 an zu zählen
 
  #--------------------------------------------------------------------------------------------------------- 
 
-#Text
 
 def print_langsam(str):                #Jeder einzelne Buichstabe innerhalb des textes mit print_langsam wird mit 0.06 Sekunden Abstand ausgegeben.
     for letter in str:
@@ -33,9 +29,7 @@ def print_schneller(str):              #Jeder einzelne Buichstabe innerhalb des 
         time.sleep(0.035)
 
 
- #---------------------------------------------------------------------------------------------------------   
- #Programm an sich
-    
+ #---------------------------------------------------------------------------------------------------------       
 os.system("cls")
 
 print_langsam("Hallo! Sie sind hier da sie ein Verdächtiger im Mordfall von Ulrike sind.")
